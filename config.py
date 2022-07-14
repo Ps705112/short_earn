4b87412640d0533a39a49df125e2dcdce6e6fb60
vaibhavp0786 <104673122+vaibhavp0786@users.noreply.github.com>
2022-07-14 18:42:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ bot = Client(
     "my-mdisk",
     api_id="8742500",
     api_hash="7ff1dec1b54f5e90d31d633a7729ad7b",
-    bot_token="5310573576:AAEacytEzyH-Mr087HiSjLEgdvYrVYlFtOQ",
+    bot_token="5579083068:AAG8RXAEzE9MmQ_B6YvETHGx0i9OJow6N5k",
 )
 
 API_ID = os.environ.get("API_ID")
